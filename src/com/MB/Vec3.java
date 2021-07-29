7bbc86e151b01c5fdf8d749f82dd34f0e6882a44
Morebits <tom_moore36@hotmail.com>
2021-07-29 20:53:42 +0100

    Update Vec3.java
    more methods added

--- src/com/MB/Vec3.java
+++ src/com/MB/Vec3.java
@@ -4,11 +4,16 @@ package com.MB;
 import java.util.Objects;
 
 public class Vec3{
-    public int X = -1;
-    public int Y = -1;
-    public int Z = -1;
-    public Vec3(int x, int y, int z){
+    public float X = -1;
+    public float Y = -1;
+    public float Z = -1;
+    public Vec3(float x, float y, float z){
         X = x;Y=y;Z=z;
+    }
+    public Vec3(){
+        X = 0;
+        Y = 0;
+        Z = 0;
     }
 
     @Override
@@ -18,7 +23,7 @@ public class Vec3{
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+       if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Vec3 vec3 = (Vec3) o;
         return X == vec3.X && Y == vec3.Y && Z == vec3.Z;
@@ -31,17 +36,7 @@ public class Vec3{
 
 
     public Vec3 add(Vec3 other){
-        Vec3 tempVec3 =  new Vec3(X + other.X,Y + other.Y,Z + other.Z);
-        if (tempVec3.X < -1000)
-            tempVec3.X = 1;
-
-        if (tempVec3.Y < -1000)
-            tempVec3.Y = 1;
-
-        if (tempVec3.Z < -1000)
-            tempVec3.Z = 1;
-
-        return tempVec3;
+        return new Vec3(X + other.X,Y + other.Y,Z + other.Z);
     }
 
     public Vec3 subtract(Vec3 other) {
@@ -58,8 +53,40 @@ public class Vec3{
         return new Vec3((int) (X * t),(int)(Y*t),(int)(Z*t));
     }
 
-    public Vec3 addEquals(Vec3 other) {
-        return new Vec3(X+=other.X,Y+=other.Y,Z+=other.Z);
 
+    public Vec3 addEquals(Vec3 other) {
+        this.X += other.X;
+        this.Y += other.Y;
+        this.Z += other.Z;
+        return this;
+    }
+
+    public Vec3 scaleEquals(Vec3 other) {
+        this.X *= other.X;
+        this.Y *= other.Y;
+        this.Z *= other.Z;
+        return this;
+    }
+
+    public Vec3 divideEqual(Vec3 other){
+        this.X /= other.X;
+        this.Y /= other.Y;
+        this.Z /= other.Z;
+        return this;
+    }
+
+    public Vec3 subtractEquals(Vec3 other) {
+        this.X -= other.X;
+        this.Y -= other.Y;
+        this.Z -= other.Z;
+        return this;
+    }
+
+    public float lengthSquared() {
+        return (float)  (X*X) + (Y*Y) + (Z*Z)  ;
+    }
+
+    public float length() {
+        return (float) Math.sqrt(lengthSquared());
     }
 }
