983cfa0f40b887510c2ffa0fb05afe4d880e4a6e
Morebits <tom_moore36@hotmail.com>
2022-02-26 19:52:42 +0000

    main method changed to work

--- src/com/MB/Main.java
+++ src/com/MB/Main.java
@@ -5,7 +5,7 @@ public class Main {
     /** Main. **
      *  * @param args - arguments for the function.
      */
-    public void main(String[] args) {
+    public static void main(String[] args) {
         App app = new App();
         app.run();
     }
